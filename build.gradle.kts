bef340a97eb8817d1af3c1599ebf4817396621be
james <james-gibson@ntlworld.com>
2022-01-02 15:52:42 +0000

    Added application to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    application
 }
 
 group = "me.james"
@@ -24,4 +25,7 @@ tasks.test {
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+application{
+    mainClass.set("MainKt")
 }
